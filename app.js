f9840aa5a75153aaf28356552f553a8dd844a418
Masum Billah <office.hellomasum@gmail.com>
2023-09-09 13:05:02 -0700

    JS added for main collection color div

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 (function($){
+
+// Main Collection Color Div
+
+var color_replace = $('#color_replace');
+var color_div = $('.color_div');
+
+color_div.each(function (){
+    $(color_div).on('click',color_div, function(){
+        var color_name = $(this).data('color');
+        console.log(color_name)
+    });
+});
+
 //Pillowcase Modal Images Showing
-// var single_img = $('.single_item_gallery img');
-
-// $(single_img).on('click', single_img, function (){
-//     var src = $(this).attr('src');
-//     $('#gallery_display').attr('src', src);
-// });
-
 
 var single_img = $('.single_item_gallery img');
 single_img.each(function () {
@@ -17,6 +23,8 @@ single_img.each(function () {
        
     });
 });
+
+
 
 
 })(jQuery);

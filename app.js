aae11f671cc587c102d27268e6569c5b9d0a6c39
Masum Billah <office.hellomasum@gmail.com>
2023-09-10 07:16:51 -0700

    Main Color Hover + Click Effect Done

--- app.js
+++ app.js
@@ -5,13 +5,16 @@
 var color_div = $('.singel_color_div');
 
 color_div.each(function (){
-    $(this).on('click', function(){
+    $(this).on('click mouseover', function(){
         var color_name = $(this).data('color');
         var parentElement = $(this).parent();
-        var color_replace = parentElement.closest('#color_replace');
-        // color_replace.html(color_name);
-        console.log(color_replace)
-        color_replace.html('jkdf')
+
+        var color_replace_div = parentElement.prev('p').find('strong#color_replace');
+        color_replace_div.text(color_name);
+
+        // console.log(color_replace_div)
+        
+    
             
     });
 });

37929d6c1ed0adef05da5ef1a4f1271c062aab6a
Masum Billah <office.hellomasum@gmail.com>
2023-09-10 09:26:45 -0700

    Color Swtiches & Block Color Div Toggle done

--- app.js
+++ app.js
@@ -8,18 +8,13 @@ color_div.each(function (){
     $(this).on('click mouseover', function(){
         var color_name = $(this).data('color');
         var parentElement = $(this).parent();
-
         var color_replace_div = parentElement.prev('p').find('strong#color_replace');
         color_replace_div.text(color_name);
-
-        // console.log(color_replace_div)
-        
-    
             
     });
 });
 
-//Pillowcase Modal Images Showing
+//Block Modal Images Showing
 
 var single_img = $('.single_item_gallery img');
 single_img.each(function () {
@@ -31,6 +26,31 @@ single_img.each(function () {
     });
 });
 
+
+// Block Color Swtiches Div
+
+var block_color_swtiches_div = $('.block_color_swtiches_div');
+var color_swtiches = $('.color_swtiches');
+color_swtiches.each(function () {
+    $(color_swtiches).on('click', function (){
+       $(this).closest('div').find(block_color_swtiches_div).toggle();
+    });
+});
+
+
+// Block Color Swtiches
+
+var color_div = $('.singel_color_div');
+
+color_div.each(function (){
+    $(this).on('click mouseover', function(){
+        var color_name = $(this).data('color');
+        var parentElement = $(this).parent().parent();
+        var color_replace_div = parentElement.prev('p').find('strong#color_replace');
+        color_replace_div.text(color_name);
+            
+    });
+});
 
 
 

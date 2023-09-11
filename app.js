75c4f4b8e4d7a85a5e634098da2ff6853451a717
Masum Billah <office.hellomasum@gmail.com>
2023-09-11 09:45:06 -0700

    Images Swtiches for Pillowcase done

--- app.js
+++ app.js
@@ -40,9 +40,12 @@ var color_div = $('.singel_color_div');
 color_div.each(function (){
     $(this).on('click mouseover', function(){
         var color_name = $(this).data('color');
+        var img_url = $(this).data('img-url');
+        var testID = $('#testID');
         var parentElement = $(this).parent().parent();
         var color_replace_div = parentElement.prev('p').find('strong#color_replace');
         color_replace_div.text(color_name);
+        testID.attr('src', img_url);
             
     });
 });

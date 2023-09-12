de1e8aae2b5061cc5792021737c3f1989aab8fe9
Masum Billah <office.hellomasum@gmail.com>
2023-09-12 07:34:31 -0700

    Dynamic all images swtiches done

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 (function($){
 
 // Main Collection Color Div
-var color_div = $('.singel_color_div');
+var color_div = $('.main_color_div');
 color_div.each(function (){
     $(this).on('click mouseover', function(){
+
+        //Color Swtiches
         var color_name = $(this).data('color');
         var parentElement = $(this).parent();
         var color_replace_div = parentElement.prev('p').find('strong#color_replace');
         color_replace_div.text(color_name);
+
+        //Images Swtiches Bundle Thumbnail
+        var img_url = $(this).data('img-url');
+        var swtiches_target = $(this).closest('.thumb_class').find('#img_thumb');
+        swtiches_target.attr('src', img_url);
+
+        //Images Swtiches Pillowcase Thumbnail
+        var img_pillow = $(this).data('pillow');
+        var pillow_thumb = $('.pillow_thumb');
+        pillow_thumb.attr('src', img_pillow);
+
+
+         //Images Swtiches Duvet Cover Thumbnail
+         var img_duvet = $(this).data('duvet');
+         var duvet_thumb = $('.duvet_thumb');
+         duvet_thumb.attr('src', img_duvet);
+
+         //Images Swtiches Fitted Sheet Thumbnail
+         var img_fitted = $(this).data('fitted');
+         var fitted_thumb = $('.fitted_thumb');
+         fitted_thumb.attr('src', img_fitted);
+        
+        
+
+            
+    });
+});
+
+
+// Mian Color Buttons Inteaction in 3 space ( Pillwocase, Duvet Cover, Fitted Sheet)
+var color_div = $('.singel_color_div');
+color_div.each(function (){
+    $(this).on('click mouseover', function(){
+        var color_name = $(this).data('color');
+        var img_url = $(this).data('img-url');
+        var swtiches_target = $(this).closest('.thumb_class').find('#img_thumb');
+        var parentElement = $(this).parent().parent();
+        var color_replace_div = parentElement.prev('p').find('strong#color_replace');
+        color_replace_div.text(color_name);
+        swtiches_target.attr('src', img_url);
+    
             
     });
 });
@@ -35,7 +78,7 @@ color_swtiches.each(function () {
 
 
 
-// Block Color Swtiches
+// Block Color Swtiches & Image Changes
 var color_div = $('.singel_color_div');
 color_div.each(function (){
     $(this).on('click mouseover', function(){
